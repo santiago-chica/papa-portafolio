1d1fe1c75ae18eeac48a72fe7fd9ffa276f5517c
Greyberet <58717769+Greyberet@users.noreply.github.com>
2026-04-11 20:21:42 -0500

    New starting point

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-red rounded-xl shadow-lg flex items-center gap-x-4">
+      <div className="shrink-0">asd </div>
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-slate-500">You have a new message!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
